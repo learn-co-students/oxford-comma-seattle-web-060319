0007ccf9cdbba0395df62c58f0decf465aaa79d2
Dex Fitch <fitch.dexter@gmail.com>
2019-05-11 08:35:18 -0700

    Oxford comma methods work

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    phrase = ""
+    while array.length > 1
+      phrase.concat("#{array[0]}, ")
+      array.shift()
+    end
+    return phrase.concat("and #{array[0]}")
+  end 
 end
