c427cd0d56002441780529ca8a92330e25bb1b82
Jason Leach <jlspursfan@gmail.com>
2019-05-13 00:39:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    str = array[0] + " and " + array[1]
+    return str
+  else
+    str = ""
+    array.each do |ele|
+      if ele != array[-1]
+        str += ele + ", "
+      elsif ele == array[-1]
+        str += "and " + ele
+      end
+    end
+    return str
+  end
 
 end
