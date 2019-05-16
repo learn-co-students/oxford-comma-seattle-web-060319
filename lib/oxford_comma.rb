4a2251505297b821b7ea50595d0c1532a9f7b5e9
Dan-Vy Le <danvyle85@gmail.com>
2019-05-16 20:59:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2 
+    array.join(" and ")
+  else
+    new_ending = "and " + array.last 
+    array.pop
+    array << new_ending
+    array.join(", ")
+  end 
 end
+
+#I must first figure out the last element, insert "and" and then join the array into a string, separated by string. Can i find the last element and change it into "and #{element}" and then join?
